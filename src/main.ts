acfb9bcffefc43f657ca0e934d6a6e2ee011d7be
marcosmarinho <marcosmarinho19998@gmail.com>
2025-06-21 14:27:42 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { HttpServer } from './domain/interfaces/HttpServer';
 function main(): void {
   const port: number = Number(process.env.PORT ?? '3000');
   const container: Container = new Container();
-  const server: HttpServer = new FastifyHttpServer() ?? new ExpressHttpServer();
+  const server: HttpServer =  new ExpressHttpServer() ??  new FastifyHttpServer();
   const routers: Routers = new Routers(server);
   routers.register(container);
   server.listen(port);
