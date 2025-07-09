2d889ada749aca780c4922f6b31e42a9b4a506c0
marcosmarinho <marcosmarinho19998@gmail.com>
2025-07-09 12:51:00 -0300

    update

--- frontend/src/presentation/components/ErrorAlert.vue
+++ frontend/src/presentation/components/ErrorAlert.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-defineProps({message:String})
-const visible = ref<boolean>(true)
-const close = ()=> visible.value = false
+import { onMounted, ref, } from 'vue';
+defineProps({ message: String });
+const visible = ref<boolean>(true);
+const close = () => visible.value = false;
+const time = () => setTimeout(() => { visible.value = false; }, 3000);
+onMounted(() => time());
 </script>
 
 <template>
-  <div
-    v-if="visible"
+  <div v-show="visible"
     class="bg-red-500 mb-2 absolute top-50 text-white px-4 py-3 rounded flex justify-between items-center shadow-md animate-fade-in"
-    role="alert"
-  >
+    role="alert">
     <span class="font-semibold">{{ message }}</span>
     <button @click="close" class="ml-4 text-white text-xl leading-none hover:text-gray-200">
       &times;
